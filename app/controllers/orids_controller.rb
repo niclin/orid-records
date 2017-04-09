332e85921c68e7a874fe37fee0ba295c3d75c6ef
niclin <bboyceo@hotmail.com>
2017-04-10 01:02:42 +0800

    Add index page of orid

--- app/controllers/orids_controller.rb
+++ app/controllers/orids_controller.rb
@@ -1,2 +1,26 @@
 class OridsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @orids = Orid.all
+  end
+
+  def new
+    @orid = Orid.new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :orids
+
+  root 'orids#index'
 end
