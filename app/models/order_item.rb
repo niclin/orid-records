e02d41449f465ca077d26cb1dcc474e06a76b9d9
Nic <niclin0226@gmail.com>
2017-04-26 19:45:56 +0800

    Annotate rails with schema

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: order_items
+#
+#  id              :integer          not null, primary key
+#  order_id        :integer
+#  membership_name :string
+#  price           :integer
+#  quantity        :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class OrderItem < ApplicationRecord
   belongs_to :order
 end
--- app/models/orid.rb
+++ app/models/orid.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: orids
+#
+#  id           :integer          not null, primary key
+#  user_id      :integer
+#  objective    :text
+#  reflective   :text
+#  interpretive :text
+#  decisional   :text
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Orid < ApplicationRecord
   belongs_to :user
   has_many :comments
