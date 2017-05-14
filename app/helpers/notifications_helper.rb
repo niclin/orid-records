2e5b34ce61f1c99d86a3112470b0001719e30a43
niclin <bboyceo@hotmail.com>
2017-05-15 00:07:04 +0800

    Add span badge for notify if exist

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,4 +2,8 @@ module NotificationsHelper
   def render_user_profile_link(user)
     link_to user.name
   end
+
+  def render_currnet_user_notify_count
+    Notification.where(recipient: current_user).unread.count
+  end
 end
