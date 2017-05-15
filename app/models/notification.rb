09703fdfbe78d1d25ad92b975ce8dad2fab81555
Nic <bboyceo@hotmail.com>
2017-05-15 14:29:29 +0800

    Fix association bug

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,7 +2,6 @@ class Notification < ApplicationRecord
   belongs_to :recipient, class_name: "User"
   belongs_to :actor, class_name: "User"
   belongs_to :notifiable, polymorphic: true
-  belongs_to :comment
 
   has_many :notifications, as: :notifiable, dependent: :destroy
 
