f432e1914fbd9539dfeb79ba110cfb12d7d357c9
Nic <bboyceo@hotmail.com>
2017-05-18 18:47:57 +0800

    Add upload avatar path

--- app/controllers/account/users_controller.rb
+++ app/controllers/account/users_controller.rb
@@ -1,8 +1,26 @@
 class Account::UsersController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @user = current_user
+  end
+
+  def update
+    if current_user.update(user_params)
+      flash[:notice] = "信息更新成功"
+      redirect_to account_users_path
+    else
+      render :edit
+    end
+  end
+
   def show
     @orids = current_user.orids
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :avatar)
+  end
 end
