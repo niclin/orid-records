c3edf2debe0f25b58b5ee696f53411e9ca01043a
niclin <bboyceo@hotmail.com>
2017-05-20 13:39:05 +0800

    Notufy center must be login

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,6 @@
 class NotificationsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @notifications = Notification.where(recipient: current_user).order("id DESC")
     @notifications.update_all(read_at: Time.zone.now)
