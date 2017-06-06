bd2d96da43b463874891519cef354ffb2e44df16
niclin <bboyceo@hotmail.com>
2017-06-07 02:10:53 +0800

    Add orid status helper

--- app/helpers/orids_helper.rb
+++ app/helpers/orids_helper.rb
@@ -1,2 +1,6 @@
 module OridsHelper
+  def render_orid_status(orid)
+    tag = orid.status == "public" ? "label-success" : "label-default"
+    content_tag(:span, t(orid.status, :scope => "orid.status"), :class => "label #{tag}")
+  end
 end
